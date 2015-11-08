9ae30c03755a39733183d9b883a00dea2d54ccf6
fracmode <amunu.rets@gmail.com>
2015-11-08 17:10:28 +0900

    [add] add ios/swiftWithoutStoryboard project files
    add ios/swiftWithoutStoryboard project files

---
+++ ios/swiftWithoutStoryboard/swiftWithoutStoryboardTests/swiftWithoutStoryboardTests.swift
@@ -0,0 +1,36 @@
+//
+//  swiftWithoutStoryboardTests.swift
+//  swiftWithoutStoryboardTests
+//
+//  Created by Sho Terunuma on 11/8/15.
+//  Copyright © 2015 TestOrg. All rights reserved.
+//
+
+import XCTest
+@testable import swiftWithoutStoryboard
+
+class swiftWithoutStoryboardTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
