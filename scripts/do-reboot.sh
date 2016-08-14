fb9b78a08a841f675c96055e27015e5bccb242de
terunuma <amunu.rets@gmail.com>
2016-08-14 20:34:46 +0900

    send mail if system is rebooting

--- scripts/do-reboot.sh
+++ scripts/do-reboot.sh
@@ -5,6 +5,11 @@ tmpFile="/tmp/temp"`date +%Y%m%d%H%M%S`
 
 prefix="[${timestamp}] FileSystem Check:"
 
+mail_from="<MAIL FROM>"
+mail_to="<MAIL TO>"
+domain="<DOMAIN>"
+server_name="<SERVER NAME>"
+
 function check_file_system() {
     touch ${tmpFile}
 
@@ -16,11 +21,20 @@ function check_file_system() {
         echo "${prefix} FileSystem is Stable."
         rm -f ${tmpFile}
     fi
-  
 } 
 
 function error_check_file_system() {
-    echo "${prefix} FileSystem may be Read-Only. Rebooting..."
+    mail_subject="[${domain}] ${server_name} reboot on ${timestamp}"
+    mail_body="ALERT: ${mail_subject}"
+    # echo "${prefix} FileSystem may be Read-Only. Rebooting..."
+    {
+        echo "From: ${mail_from}"
+        echo "To: ${mail_to}"
+        echo "Subject: ${mail_subject}"
+        echo
+        echo "${mail_body}"
+    } | sendmail -i -f "${mail_from}" "${mail_to}"
+
     reboot
 } 
 
